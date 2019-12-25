7720316918d610104dcf368139077da3c993b1e7
weibb <876585863@qq.com>
2019-12-25 17:32:37 +0800

    create project hellogit

---
+++ src/main/java/com/weibb/base/HelloBody.java
@@ -0,0 +1,15 @@
+package com.weibb.base;
+
+/**
+ * 描述:
+ * User:Administrator
+ * Date:2019/12/25 0025
+ */
+public class HelloBody {
+
+    public static void main(String[] args) {
+        String name = "Wei Binbin";
+        System.out.println("Hello " + name);
+    }
+
+}
---
+++ target/hellogit/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello Git!</h2>
+<h2>Hello Maven!</h2>
+<h2>Hello World!</h2>
+</body>
+</html>
